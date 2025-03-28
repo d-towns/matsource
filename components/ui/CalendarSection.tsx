16e3730104a8b36f47014b014968f6bfd7ef369e
dt0wns <dennis@dennistowns.com>
2025-03-27 20:27:45 -0400

    refactor: Adjust spacing and padding in CalendarSection for improved layout
    - Reduced top padding from 24 to 12 and adjusted bottom padding for better visual balance.
    - Added horizontal padding to the description paragraph for enhanced readability.

--- components/ui/CalendarSection.tsx
+++ components/ui/CalendarSection.tsx
@@ -4,10 +4,10 @@ import CalComEmbed from "./CalComEmbed";
 
 export default function CalendarSection() {
   return (
-    <div id="contact-us" className="flex flex-col md:justify-center items-center mb-16 md:mb-24 pt-8 md:pt-24">
+    <div id="contact-us" className="flex flex-col md:justify-center items-center mb-16 md:mb-24 pt-6 md:pt-12">
         <div className="flex flex-col max-w-7xl w-full items-center justify-center">
             <h2 className="text-4xl font-bold mb-2">Lets Connect.</h2>
-            <p className="text-left text-gray-500 mb-4 md:mb-16 md:px-0 w-full md:w-1/4">If you&lsquo;re ready to explore an AI Voice solution for your business, feel free to schedule a discovery call.</p>
+            <p className="text-left px-12 text-gray-500 mb-4 md:mb-16 md:px-0 w-full md:w-1/4">If you&lsquo;re ready to explore an AI Voice solution for your business, feel free to schedule a discovery call.</p>
         </div>
         <CalComEmbed />
     </div>
